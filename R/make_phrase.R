cb4f48edb26c8377b5d856b2351a550377343c83
sdhaliwal7 <67290557+sdhaliwal7@users.noreply.github.com>
2020-08-09 22:35:08 -0700

    Add files via upload

---
+++ R/make_phrase.R
@@ -0,0 +1,27 @@
+#' Takes a noun and makes it plural
+#'
+#' @param num An integer
+#' @param num_word A string corresponding to the integer
+#' @param item A string
+#' @param verb A string
+#' @param adjective A string
+#' @param location A string
+#'
+#' @return A string containing the words in grammatical order.
+#'
+#' @import stringr
+#' @import glue
+#' @import dplyr
+#' @import purrr
+#'
+#' @export
+make_phrase <- function(num, num_word, item, verb='', adjective='', location= '') {
+  phrase <- paste(num_word, adjective, item, verb, location, sep =' ' )
+  return(phrase)
+}  
+
+
+
+
+
+
---
+++ R/pluralize_gift.R
@@ -0,0 +1,16 @@
+#' Takes a noun and makes it plural
+#'
+#' @param gift A string or vector of strings
+#'
+#' @return A string or vector of strings with the pluralized words
+#'
+#' @import stringr
+#' @import dplyr
+#' @import glue
+#' @import purrr
+#'
+#' @export
+pluralize_gift <- function(gift){
+    gift <- gift %>% str_subset("[A-z]$") %>% paste("s", sep="")
+    return(gift)
+}
